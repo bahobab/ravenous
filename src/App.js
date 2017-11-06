5bb3b23ee0cb1f43623ea4284247ecf98311cc03
mono motapa <bahobab@hotmail.com>
2017-11-05 20:45:03 -0600

    part2: props

--- src/App.js
+++ src/App.js
@@ -5,13 +5,34 @@ import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 
+const business = {
+	imgSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+	name: 'MarginOtto Pizzeria',
+	address: '1010 Padington Way',
+	city: 'Flavortown',
+	state: 'NY',
+	zipCode: '10101',
+	category: 'Italian',
+	rating: 4.5,
+	reviewCount: 90
+};
+
+let businesses = [
+	business,
+	business,
+	business,
+	business,
+	business,
+	business
+];
+
 class App extends Component {
   render() {
     return (
       <div className='App'>
 				<h1>ravenous</h1>
 				<SearchBar />
-				<BusinessList />
+				<BusinessList businesses={businesses}/>
 			</div>
     );
   }
